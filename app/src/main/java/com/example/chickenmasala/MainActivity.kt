5ce08f3c6b6f119afbc24d43b8326c2757a443c5
YaseenOmar <yz456359@gmail.com>
2023-03-20 19:23:01 +0200

    get list of based of recipes name  with filters without test

--- app/src/main/java/com/example/chickenmasala/MainActivity.kt
+++ app/src/main/java/com/example/chickenmasala/MainActivity.kt
@@ -15,20 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        //Just for testing and must be removed
-//        CsvDataSource(this, Constants.RECIPES_CSV_FILE_NAME, RecipeParser()).apply {
-//            getAllItems().forEachIndexed { index, recipeEntity ->
-//                Log.v(TAG, "[$index]: $recipeEntity")
-//            }
-//        }
 
-//        //Just for testing and must be removed [demonstration of calling an interactor]
-//        CsvDataSource(this, Constants.RECIPES_CSV_FILE_NAME, RecipeParser()).apply {
-//            GetRecipesOfCuisineInteractor(this).execute("indian",5).forEachIndexed { index, recipeEntity ->
-//                Log.v(TAG, "[$index]: $recipeEntity")
-//            }
-//        }
 
+        //Just for testing and must be removed
         CsvDataSource(this , Constants.RECIPES_CSV_FILE_NAME , RecipeParser()).apply {
             GetAListOfRecipesBasedOnRecipeNameWithFiltersAsArgumentsInteractor(this).execute(
                 ingredientsCount = 10
@@ -42,31 +31,7 @@ class MainActivity : AppCompatActivity() {
                     )
                 }
         }
-//        CsvDataSource(this, Constants.RECIPES_CSV_FILE_NAME, RecipeParser()).apply {
-//            GetAListOfRecipesBasedOnRecipeNameWithFiltersAsArgumentsInteractor(this).execute(
-//                cuisine = "indian" ,
-//                totalTimeMins = 10 ,
-////                ingredientsCount = 10,
-//
-//            )
-//                .forEachIndexed{ index , recipeEntity ->
-//                    Log.v("iii" , "[${index+1}] "
-////                            + "Name :  ${recipeEntity.name}"
-//                            + " ,Time :  ${recipeEntity.totalTime} ,"
-//                            + " Count :  ${recipeEntity.ingredientsCount} "
-//                            + " , Cuisine : ${recipeEntity.cuisine}"
-//                    )
-//                }
-//        }
 
-
-
-//        ////Just for testing and must be removed
-//        CsvDataSource(this, Constants.CATEGORIES_CSV_FILE_NAME, CategoryParser()).apply {
-//            getAllItems().forEachIndexed { index, categoryEntity ->
-//                Log.v(TAG, "[$index]: $categoryEntity")
-//            }
-//        }
 
 
     }
