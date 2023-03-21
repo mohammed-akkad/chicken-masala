9711d4688b3268ea98c46633cd4a40953bb62f39
jalal cheff <jlalalmyahy571@gmail.com>
2023-03-22 00:27:41 +0300

    rename class GuessGamesInteractor.kt

--- app/src/main/java/com/example/chickenmasala/data/domain/GuessGamesName.kt
+++ app/src/main/java/com/example/chickenmasala/data/domain/GuessGamesName.kt
@@ -1,6 +1,6 @@
 package com.example.chickenmasala.data.domain
 
-enum class GuessGamesNameInteractor {
+enum class GuessGamesName {
     GUESS_THE_EXSTING_INGREDIENT,
     GUESS_THE_CUISINE,
     GUESS_THE_MEAL,
