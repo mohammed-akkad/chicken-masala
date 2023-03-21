72e093dc3c376ea7ee32d8df6c21a3723ad5d560
azmoh09 <momoo362514@gmail.com>
2023-03-21 22:10:19 +0200

    update xml file

--- app/src/main/java/com/example/chickenmasala/SearchEoodFragment.kt
+++ app/src/main/java/com/example/chickenmasala/SearchEoodFragment.kt
@@ -34,7 +34,7 @@ class SearchEoodFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_eood, container, false)
+        return inflater.inflate(R.layout.fragment_search_food, container, false)
     }
 
     companion object {
