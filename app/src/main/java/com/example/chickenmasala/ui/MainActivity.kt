9462fc353876c52b9392aac5d7f868ed84b9d883
azmoh09 <momoo362514@gmail.com>
2023-03-22 17:28:39 +0200

    complete fragment class

---
+++ app/src/main/java/com/example/chickenmasala/ui/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.chickenmasala.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.chickenmasala.R
+import com.example.chickenmasala.databinding.ActivityMainBinding
+import com.example.chickenmasala.util.Operation
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MAIN_ACTIVITY"
+
+    lateinit var binding: ActivityMainBinding
+    val homeFragment = HomeFragment()
+    val kitchenFragment = KitchenFragment()
+    val triviaGamesFragment = TriviaGamesFragment()
+    val indianFoodHistoryFragment = IndianFoodHistoryFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        addCallBacks()
+
+
+    }
+
+    private fun addCallBacks() {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home_screen -> {
+                    showFragment(homeFragment)
+                    true
+                }
+                R.id.kitchen_screen -> {
+                    showFragment(kitchenFragment)
+                    true
+                }
+                R.id.trivia_screen -> {
+                    showFragment(triviaGamesFragment)
+                    true
+                }
+                R.id.indian_food_history -> {
+                    showFragment(indianFoodHistoryFragment)
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+}
