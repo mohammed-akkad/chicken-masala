1ea8bd6a6523dd8d085aaa5c3a0f319300c9ff96
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-03-27 16:50:34 +0300

    initialize Transition keys

--- app/src/main/java/com/example/chickenmasala/util/Constants.kt
+++ app/src/main/java/com/example/chickenmasala/util/Constants.kt
@@ -3,4 +3,15 @@ package com.example.chickenmasala.util
 object Constants {
     const val RECIPES_CSV_FILE_NAME = "indian_food.csv"
     const val CATEGORIES_CSV_FILE_NAME = "categories.csv"
+
+    object TransitionKeys{
+        const val SELECTED_IMAGE_FOOD = "img_selectedFood"
+        const val SELECTED_FOOD_NAME = "txt_FoodName"
+        const val SELECTED_FOOD_DETAIL_NAME = "txt_FoodDetailName"
+        const val SELECTED_FOOD_DESCRIPTION = "txt_FoodDescription"
+        const val SELECTED_FIRST_RELATIVE_FOOD_NAME = "txt_FirstRelativeFoodName"
+        const val SELECTED_SECOND_RELATIVE_FOOD_NAME = "txt_SecondRelativeFoodName"
+        const val SELECTED_FIRST_RELATIVE_FOOD_IMAGE = "img_FirstRelativeFoodImage"
+        const val SELECTED_SECOND_RELATIVE_FOOD_IMAGE = "img_SecondRelativeFoodImage"
+    }
 }
