5171b9823e3c7f7891d944d920fe55d5de218cac
mhmoudhabib <mhmoudrhabib@gmail.com>
2023-03-28 21:34:34 +0200

    add sample code to show how to read the recipes list in FoodKitchenCategoryFragment

--- app/src/main/java/com/example/chickenmasala/ui/FoodKitchenCategoryFragment.kt
+++ app/src/main/java/com/example/chickenmasala/ui/FoodKitchenCategoryFragment.kt
@@ -1,8 +1,12 @@
 package com.example.chickenmasala.ui
 
+import android.os.Build
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.example.chickenmasala.data.domain.RecipeEntity
 import com.example.chickenmasala.databinding.FragmentFoodKitchenCategoryBinding
+import com.example.chickenmasala.util.Constants
 
 
 class FoodKitchenCategoryFragment : BaseFragment<FragmentFoodKitchenCategoryBinding>() {
@@ -12,7 +16,16 @@ class FoodKitchenCategoryFragment : BaseFragment<FragmentFoodKitchenCategoryBind
         FragmentFoodKitchenCategoryBinding::inflate
 
     override fun setup() {
-
+        arguments?.let {
+            val list = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                it.getParcelableArrayList(Constants.KEY_RECIPES_LIST,RecipeEntity::class.java)?.toList()
+            } else {
+                it.getParcelableArrayList<RecipeEntity>(Constants.KEY_RECIPES_LIST)?.toList()
+            }
+            list?.forEach {recipe->
+                Log.e(LOG_TAG, "item: $recipe", )
+            }
+        }
     }
 
     override fun addCallBacks() {
