7f5811cfe5f3e21274655a7507c84fe691dc6c64
Mohamed Emad <asd55asd36@gmail.com>
2023-03-28 23:25:38 +0200

    remove unused imports

--- app/src/main/java/com/example/chickenmasala/ui/GuessTheMealFragment.kt
+++ app/src/main/java/com/example/chickenmasala/ui/GuessTheMealFragment.kt
@@ -1,8 +1,6 @@
 package com.example.chickenmasala.ui
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
