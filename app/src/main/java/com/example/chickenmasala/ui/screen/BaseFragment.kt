5d40852fc50abe3e3889d02667ef5d769e81fbff
azmoh09 <momoo362514@gmail.com>
2023-03-28 20:01:20 +0300

    improve style code

--- app/src/main/java/com/example/chickenmasala/ui/screen/BaseFragment.kt
+++ app/src/main/java/com/example/chickenmasala/ui/screen/BaseFragment.kt
@@ -8,20 +8,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<VB:ViewBinding>:Fragment(){
-abstract val LOG_TAG:String
-abstract val bindingInflater:(LayoutInflater,ViewGroup?,Boolean)->VB
-    private var _binding:ViewBinding?=null
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+    abstract val LOG_TAG: String
+    abstract val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
+    private var _binding: ViewBinding? = null
     protected val binding
-    get() = _binding as VB
+        get() = _binding as VB
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        setup()
-
-        _binding=bindingInflater(inflater,container,false)
+        _binding = bindingInflater(inflater, container, false)
 
         return (_binding as VB).root
 
@@ -32,9 +31,10 @@ abstract val bindingInflater:(LayoutInflater,ViewGroup?,Boolean)->VB
         setup()
         addCallBacks()
     }
+
     abstract fun setup()
     abstract fun addCallBacks()
 
-    protected fun log(value: String)=Log.v(LOG_TAG,value)
+    protected fun log(value: String) = Log.v(LOG_TAG, value)
 
 }
--- app/src/main/java/com/example/chickenmasala/ui/screen/GuessTheIngredientFragment.kt
+++ app/src/main/java/com/example/chickenmasala/ui/screen/GuessTheIngredientFragment.kt
@@ -4,8 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.chickenmasala.databinding.FragmentGuessTheIngredientBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 class GuessTheIngredientFragment : BaseFragment<FragmentGuessTheIngredientBinding>() {
     override val LOG_TAG: String = "GuessTheIngredientFragment"
