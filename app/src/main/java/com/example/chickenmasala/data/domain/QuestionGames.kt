ef8f0a993a440c19126144ca232baa47c22b783f
jalal cheff <jlalalmyahy571@gmail.com>
2023-04-01 01:12:20 +0300

    edit in QuestionGames.kt data class

--- app/src/main/java/com/example/chickenmasala/data/domain/QuestionGames.kt
+++ app/src/main/java/com/example/chickenmasala/data/domain/QuestionGames.kt
@@ -3,5 +3,7 @@ package com.example.chickenmasala.data.domain
 data class QuestionGames(
     val question:String,
     val correctAnswer:String,
-    val incorrectAnswers:List<String>
+    val wrongAnswer1:String,
+    val wrongAnswer2:String,
+    val wrongAnswer3:String
 )
